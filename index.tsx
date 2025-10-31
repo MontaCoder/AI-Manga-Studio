ce6d3dc7626bc459994629982b6d2afb2793358c
MontaCoder <89592438+MontaCoder@users.noreply.github.com>
2025-10-31 16:45:04 +0100

    Wrap app with BrowserRouter for routing
    Added BrowserRouter to the root render tree to enable client-side routing capabilities throughout the application.

--- index.tsx
+++ index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import { LocalizationProvider } from './contexts/LocalizationContext';
 
@@ -11,8 +12,10 @@ if (!rootElement) {
 const root = ReactDOM.createRoot(rootElement);
 root.render(
   <React.StrictMode>
-    <LocalizationProvider>
-        <App />
-    </LocalizationProvider>
+    <BrowserRouter>
+      <LocalizationProvider>
+          <App />
+      </LocalizationProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
